9c93a9d5e763a5ecae9d021f1fad72fc119d423d
David Silgado Salcedo <dsilgadosalcedo@gmail.com>
2025-06-11 12:10:14 -0500

    Add configuration files and update package manager in package.json

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,20 +2,20 @@ import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
-} from '@react-navigation/native';
-import { Fragment, useEffect } from 'react';
+} from "@react-navigation/native";
+import { useEffect } from "react";
 
-import { useFonts } from 'expo-font';
-import { Slot } from 'expo-router';
+import { useFonts } from "expo-font";
+import { Slot } from "expo-router";
 
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider } from '@ui-kitten/components';
+import * as eva from "@eva-design/eva";
+import { ApplicationProvider } from "@ui-kitten/components";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -23,7 +23,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -37,15 +37,11 @@ export default function RootLayout() {
   }
 
   return (
-    <Fragment>
-      <ApplicationProvider {...eva} theme={eva.light}>
-        <ThemeProvider
-          value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
-        >
-          <Slot />
-          <StatusBar style="auto" />
-        </ThemeProvider>
-      </ApplicationProvider>
-    </Fragment>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Slot />
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </ApplicationProvider>
   );
 }
